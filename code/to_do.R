508d92b12545b33885060cc114639cae625c82c9
EisenRa <raph.eisenhofer@gmail.com>
2025-02-12 15:02:59 +0100

    update with SMF filtered samples <30%

--- code/to_do.R
+++ code/to_do.R
@@ -5,8 +5,10 @@
 # group func phylo tree by gift level (elem2function)
 # document files that are in .Rdata in the readme.md
 # timetree in repo?
+build a map of samples + metadata labels
 lab / geograph / sample metadata -> check outliers
 host life history/diets -> make table (check database)
+
 gifts -> community-weighted per species
 Clusters of bact. genomes across host species (MASH / validate with mapping results)
 networks of functions/species
